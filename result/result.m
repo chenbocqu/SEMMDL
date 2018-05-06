37d469a7ca612d09100d624279b9751b52fa0a6b
chenbocqu <chenbocqu@163.com>
2018-05-06 23:41:34 +0800

    Revert "修改目录结构名为SEMMDL"
    This reverts commit 3e2b36601cc5ab1614284544492d573b42ce250e.

---
+++ result/result.m
@@ -0,0 +1,108 @@
+
+%% 97.205% vs 96.951%
+lambda1         = 2e-3;
+
+param.rdim      = 400;
+param.f         = num_atom_per_class;
+param.max_iters = 25;
+param.lambda1   = lambda1;
+param.lambda2   = 1.8;
+param.lambda3   = 1e-6;
+param.theta     = 7; % 7 - > 97.1%
+param.draw      = true;
+
+%% 97.104%
+lambda1         = 2e-3;
+
+param.rdim      = 400; % 实验1,400，97.002%，320 97.104% 实验2,400维 20 iter 95.42%
+param.f         = num_atom_per_class;
+param.max_iters = 20;
+param.lambda1   = lambda1;
+param.lambda2   = 1.5;
+param.lambda3   = 1e-6;
+param.theta     = 7; % 7 - > 97.1%
+
+%% 96.951%
+lambda1         = 2e-3;
+
+param.rdim      = 400; % 实验1,400，97.002%，320 97.104% 实验2,400维 20 iter 95.42%
+param.f         = num_atom_per_class;
+param.max_iters = 10;
+param.lambda1   = lambda1;
+param.lambda2   = 1.5;
+param.lambda3   = 1e-6;
+param.theta     = 7;
+
+
+
+
+%% 96.9%
+lambda1         = 2e-3;
+
+param.rdim      = 300; % 实验1,400，97.002%，320 97.104% 实验2,400维 20 iter 95.42%
+param.f         = num_atom_per_class;
+param.max_iters = 35;
+param.lambda1   = lambda1;
+param.lambda2   = 1.5;
+param.lambda3   = 1e-6;
+param.theta     = 7; % 7 - > 97.1%
+
+%% 96.087%,96.037% SVGD , rdim = 150, max_iter=20
+param.rdim      = 150;
+param.f         = num_atom_per_class;
+param.max_iters = 25;
+param.lambda1   = lambda1;
+param.lambda2   = 2.5;
+param.lambda3   = 1e-6;
+param.theta     = 5; % 7 - > 97.1%
+
+%% 95.732%
+lambda1         = 2e-3;
+
+param.rdim      = 150;
+param.f         = num_atom_per_class;
+param.max_iters = 20;
+param.lambda1   = lambda1;
+param.lambda2   = 1.5;
+param.lambda3   = 1e-6;
+param.theta     = 7; % 7 - > 97.1%
+
+%% 95.478%
+lambda1         = 2e-3;
+
+param.rdim      = 150;
+param.f         = num_atom_per_class;
+param.max_iters = 25;
+param.lambda1   = lambda1;
+param.lambda2   = 1.8;
+param.lambda3   = 1e-6;
+param.theta     = 5; % 7 - > 97.1%
+
+%% 95.63%
+lambda1         = 2e-3;
+
+param.rdim      = 150;
+param.f         = num_atom_per_class;
+param.max_iters = 25;
+param.lambda1   = lambda1;
+param.lambda2   = 1.8;
+param.lambda3   = 1e-6;
+param.theta     = 5; % 7 - > 97.1%
+
+
+
+%% AR Face
+%% 95.136% vs 94.993%
+lambda1         = 2e-3;
+
+param.rdim      = 400;
+param.f         = num_atom_per_class;
+param.max_iters = 25;
+param.lambda1   = lambda1;
+param.lambda2   = 2;
+param.lambda3   = 1e-6;
+param.theta     = 7; % 7 - > 97.205%
+param.draw      = true;
+
+
+
